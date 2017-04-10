fd5c9c47c33fcc9910a5a170379f6ae2c7447fd8
Vamsi Punna <vmzi@MacBook-Pro.local>
2017-04-10 00:18:23 -0400

    Added twilio related cloud functions

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,52 @@
+var twilio = require('twilio')('ACbdcc4de40bf470456400759ca79ae61f', '831b47ca0f6698d20d344670dfc45104');
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+
+Parse.Cloud.define("sendVerificationCode", function(request, response) {
+    var verificationCode = Math.floor(Math.random()*999999);
+    var user = request.user;
+    user.set("phoneVerificationCode", verificationCode);
+    user.save();
+    
+    twilio.sendSms({
+        From: "+1786420493",
+        To: request.params.phoneNumber,
+        Body: "Your verification code is " + verificationCode + "."
+    }, function(err, responseData) { 
+        if (err) {
+          response.error(err);
+        } else { 
+          response.success("Success");
+        }
+    });
+});
+
+
+Parse.Cloud.define("verifyPhoneNumber", function(request, response) {
+    var user = request.user;
+    var verificationCode = user.get("phoneVerificationCode");
+    if (verificationCode == request.params.phoneVerificationCode) {
+        user.set("phoneNumber", request.params.phoneNumber);
+        user.save();
+        response.success("Success");
+    } else {
+        response.error("Invalid verification code.");
+    }
+});
+
+// Sample new Parse Server Cloud Code
+/*Parse.Cloud.define('getMessagesForUser', function(request, response) {
+  var user = request.user; // request.user replaces Parse.User.current()
+  var token = user.getSessionToken(); // get session token from request.user
+
+  var query = new Parse.Query('Messages');
+  query.equalTo('recipient', user);
+  query.find({ sessionToken: token }) // pass the session token to find()
+    .then(function(messages) {
+      response.success(messages);
+    });
+});*/
