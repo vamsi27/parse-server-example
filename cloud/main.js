04f88c060bc97260ffe5d1aaa10b9d5f1b7670a7
Vamsi Punna <vmzi@MacBook-Pro.local>
2017-04-11 01:30:00 -0400

    mainjs edit

--- cloud/main.js
+++ cloud/main.js
@@ -11,7 +11,7 @@ Parse.Cloud.define("sendVerificationCode", function(request, response) {
     
     
     twilio.sendSms({
-        From: "001786420493",
+        From: "7864204937",
         To: request.params["phoneNumber"],
         Body: "Your verification code is " + verificationCode + "."
     }, function(err, responseData) { 
