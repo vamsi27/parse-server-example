c71917ed7406ba751582592c7e071cbac143d4b9
Vamsi Punna <vmzi@MacBook-Pro.local>
2017-05-21 01:27:57 -0400

    changed logic to check for empty objects

--- cloud/main.js
+++ cloud/main.js
@@ -95,7 +95,7 @@ Parse.Cloud.define("addMembersToTask", function(request, response) {
           userQuery.equalTo("username", members[i]);  
           userQuery.find({
             success: function(u) {
-              if (u !== null && u !== undefined){
+              if (!isEmpty(u)){
               // Do stuff
               task.add("Members", u);
               task.save();
@@ -136,6 +136,30 @@ Parse.Cloud.define("verifyPhoneNumber", function(request, response) {
     }
 });
 
+function isEmpty(obj) {
+
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // If it isn't an object at this point
+    // it is empty, but it can't be anything *but* empty
+    // Is it empty?  Depends on your application.
+    if (typeof obj !== "object") return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
 
 /*
     // fetch users
