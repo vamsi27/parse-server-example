e8e1bdceff646d391d481d47c0f83cdd24a750bf
Vamsi Punna <vmzi@MacBook-Pro.local>
2017-06-04 18:00:42 -0400

    Formatting chnages - 'beautified' (auto indented using an online tool)

--- cloud/main.js
+++ cloud/main.js
@@ -1,28 +1,204 @@
 var twilio = require('twilio')('ACbdcc4de40bf470456400759ca79ae61f', '831b47ca0f6698d20d344670dfc45104');
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+    res.success('Hi');
 });
 
-
-
 Parse.Cloud.define("sendVerificationCode", function(request, response) {
-    
+
     // makes sure the code is always 5 digits
-    var verificationCode = 10000 + Math.floor(Math.random()*89999);
+    var verificationCode = 10000 + Math.floor(Math.random() * 89999);
 
     twilio.sendSms({
         From: "7864204937",
         To: request.params["phoneNumber"],
         Body: "Your verification code is " + verificationCode
-    }, function(err, responseData) { 
+    }, function(err, responseData) {
         if (err) {
-          response.error(err);
-        } else { 
-          response.success(verificationCode);
+            response.error(err);
+        } else {
+            response.success(verificationCode);
         }
     });
 });
+
+Parse.Cloud.define("deleteUserFromTask", function(request, response) {
+
+    var taskId = request.params["taskId"]
+    var userId = request.params["userId"]
+
+    var Task = Parse.Object.extend("Task");
+    var query = new Parse.Query(Task);
+
+    query.get(taskId, {
+        success: function(task) {
+
+            var members = task.get("Members");
+
+            if (members.length == 1) {
+                //destroy the task
+                task.destroy({
+                    success: function(t) {
+                        console.log('Task ' + t.id + ' deleted successfully')
+                    },
+                    error: function(myObject, error) {
+                        console.log('Failed to delete Task ' + t.id)
+                    }
+                });
+            } else {
+                var newAdminId = members[1].id
+
+                var userQuery = new Parse.Query(Parse.User);
+
+                userQuery.get(userId, {
+                    success: function(u) {
+
+                        //fetch current user, and remove from members
+                        task.remove("Members", u);
+
+                        //set nextusername/member to nil if it's pointing to current user
+                        if (task.get("NextTurnUserName") == u.get("username")) {
+                            console.log('Removing current user from next turn')
+                            task.unset("NextTurnUserName");
+                            task.unset("NextTurnMember");
+                        }
+
+                        //take next member and make him admin
+                        task.set("Admin", Parse.User.createWithoutData(newAdminId));
+
+                        task.save();
+                        console.log('Member ' + u.get("username") + ' removed successfully from task ' + task.id + ' and user ' + newAdminId + ' is the new admin')
+
+                    },
+                    error: function(object, error) {
+                        console.log('Member fetch error -> ' + error.message);
+                        response.error('Member fetch error -> ' + error.message);
+                    }
+                });
+            }
+
+            response.success('task found and user removed')
+
+        },
+        error: function(object, error) {
+            response.error('User could not be removed from the task')
+        }
+    });
+});
+
+Parse.Cloud.define("addMembersToTask", function(request, response) {
+
+    var taskId = request.params["tskId"]
+    var members = request.params["tskMembers"] //members usernames actually
+
+    console.log('Task id is -> ' + taskId);
+
+    var Task = Parse.Object.extend("Task");
+    var query = new Parse.Query(Task);
+
+    query.get(taskId, {
+
+        success: function(task) {
+            console.log('Task found')
+            response.success('Task found - YAY!!!')
+            // The object was retrieved successfully.
+
+            for (i = 1; i < members.length; i++) {
+                var memUName = members[i];
+
+                var userQuery = new Parse.Query(Parse.User);
+                userQuery.equalTo("username", members[i]);
+
+                console.log('^^^^^^^^^^^^^^^^^^ ' + members[i])
+
+                userQuery.first({
+                    success: function(u) {
+                        if (!isEmpty(u)) {
+                            // you can add to array by directly passing in object    
+                            u.add("Tasks", task);
+                            u.save(null, {
+                                useMasterKey: true
+                            });
+                            console.log('Added Task to User')
+
+                            // or u can add by creating empty object with id - both ways only pointer gets saved in array
+                            task.add("Members", Parse.User.createWithoutData(u.id));
+                            task.save();
+                            console.log('Member added successfully')
+                            response.success('Member added successfully')
+                        } else {
+                            console.log('Member not found - Need to create new user');
+                            createNewParseUser(memUName, task);
+                            response.error('Member not found but account may have been created -> ' + error.message);
+                        }
+                    },
+                    error: function(object, error) {
+                        console.log('Member fetch error -> ' + error.message);
+                        response.error('Member fetch error -> ' + error.message);
+                    }
+                });
+
+            }
+        },
+        error: function(object, error) {
+            console.log('Task fetch error ' + error.message);
+            response.error('Task fetch error ' + error.message);
+        }
+    });
+});
+
+function createNewParseUser(username, task) {
+    console.log('Username ----->>>>> ' + username)
+    var user = new Parse.User();
+    user.set("username", username);
+    user.set("password", username);
+    user.add("Tasks", task)
+
+    user.signUp(null, {
+        success: function(user) {
+            console.log('Account for member created successfully -> ')
+
+            user.add("Tasks", task);
+            u.save(null, {
+                useMasterKey: true
+            });
+            console.log('Added Task to User')
+
+            task.add("Members", user);
+            task.save();
+            console.log('Member added to task')
+        },
+        error: function(user, error) {
+            console.log("Sorry! Couldn't signup the user -> " + error.message)
+            //response.error("Sorry! Culdn't signup the user -> " + error.message);
+        }
+    });
+}
+
+function isEmpty(obj) {
+
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0) return false;
+    if (obj.length === 0) return true;
+
+    // If it isn't an object at this point
+    // it is empty, but it can't be anything *but* empty
+    // Is it empty?  Depends on your application.
+    if (typeof obj !== "object") return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
 
 /*
 Parse.Cloud.define("createNewUser", function(request, response) {
@@ -71,139 +247,6 @@ Parse.Cloud.define("createNewUser", function(request, response) {
     //response.success('Pass = ' + success.toString() + ' Fail = ' + fails.toString())
 
 });
-*/
-Parse.Cloud.define("deleteUserFromTask", function(request, response) {
-
-  var taskId = request.params["taskId"]
-  var userId = request.params["userId"]
-
-  var Task = Parse.Object.extend("Task");
-  var query = new Parse.Query(Task);
-  
-  query.get(taskId, {
-      success: function(task) {
-
-        var members = task.get("Members");
-
-        if(members.length == 1){
-          //destroy the task
-          task.destroy({
-          success: function(t) {
-            console.log('Task ' + t.id + ' deleted successfully')
-          },
-          error: function(myObject, error) {
-            console.log('Failed to delete Task ' + t.id)
-          }
-          });
-        }
-        else{
-          var newAdminId = members[1].id
-
-          var userQuery = new Parse.Query(Parse.User);
-          
-          userQuery.get(userId, {
-            success: function(u) {
-              
-              //fetch current user, and remove from members
-              task.remove("Members", u);
-              
-              //set nextusername/member to nil if it's pointing to current user
-              if(task.get("NextTurnUserName") == u.get("username")){
-                console.log('Removing current user from next turn')
-                task.unset("NextTurnUserName");
-                task.unset("NextTurnMember");
-              }
-
-              //take next member and make him admin
-              task.set("Admin", Parse.User.createWithoutData(newAdminId));
-
-              task.save();
-              console.log('Member ' + u.get("username") + ' removed successfully from task ' + task.id + ' and user ' + newAdminId + ' is the new admin')
-
-            },
-            error: function(object, error) {
-                
-                console.log('Member fetch error -> ' + error.message);
-                response.error('Member fetch error -> ' + error.message);
-                
-            }
-          });
-
-        }
-
-        response.success('task found and user removed')
-
-      },
-      error: function(object, error) {
-        response.error('User could not be removed from the task')
-      }
-    });
-});
-
-Parse.Cloud.define("addMembersToTask", function(request, response) {
-    
-    var taskId = request.params["tskId"]
-    var members = request.params["tskMembers"] //members usernames actually
-
-    console.log('Task id is -> ' + taskId);
-    
-    var Task = Parse.Object.extend("Task");
-    var query = new Parse.Query(Task);
-    
-    query.get(taskId, {
-      
-      success: function(task) {
-        console.log('Task found')
-        response.success('Task found - YAY!!!')
-        // The object was retrieved successfully.
-
-        for(i = 1; i < members.length; i++){
-          var memUName = members[i];
-
-          var userQuery = new Parse.Query(Parse.User);
-          userQuery.equalTo("username", members[i]); 
-
-          console.log('^^^^^^^^^^^^^^^^^^ ' + members[i])
-
-
-          userQuery.first({
-            success: function(u) {
-              if (!isEmpty(u)){
-
-              // you can add to array by directly passing in object    
-              u.add("Tasks", task);
-              u.save(null, {useMasterKey:true});
-              console.log('Added Task to User')
-
-              // or u can add by creating empty object with id - both ways only pointer gets saved in array
-              task.add("Members", Parse.User.createWithoutData(u.id));
-              task.save();
-              console.log('Member added successfully')
-              response.success('Member added successfully')
-            }
-            else{
-              console.log('Member not found - Need to create new user');
-              createNewParseUser(memUName, task);
-              response.error('Member not found but account may have been created -> ' + error.message);
-              
-          }
-            },
-            error: function(object, error) {
-                
-                console.log('Member fetch error -> ' + error.message);
-                response.error('Member fetch error -> ' + error.message);
-                
-            }
-          });
-
-        }
-      },
-      error: function(object, error) {
-        console.log('Task fetch error ' + error.message);
-        response.error('Task fetch error ' + error.message);
-      }
-    });
-});
 
 Parse.Cloud.define("verifyPhoneNumber", function(request, response) {
     var user = request.user;
@@ -217,55 +260,4 @@ Parse.Cloud.define("verifyPhoneNumber", function(request, response) {
     }
 });
 
-
-function createNewParseUser(username, task){
-
-console.log('Username ----->>>>> ' + username)
-          var user = new Parse.User();
-          user.set("username", username);
-          user.set("password", username);
-          user.add("Tasks", task)
-
-          user.signUp(null, {
-              success: function(user) {       
-              console.log('Account for member created successfully -> ')
-
-              user.add("Tasks", task);
-              u.save(null, {useMasterKey:true});
-              console.log('Added Task to User')
-
-
-              task.add("Members", user);
-              task.save();
-              console.log('Member added to task')
-          },
-          error: function(user, error) {
-            console.log("Sorry! Couldn't signup the user -> " + error.message)
-              //response.error("Sorry! Culdn't signup the user -> " + error.message);
-          } });
-}
-
-function isEmpty(obj) {
-
-    // null and undefined are "empty"
-    if (obj == null) return true;
-
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
-
-    // If it isn't an object at this point
-    // it is empty, but it can't be anything *but* empty
-    // Is it empty?  Depends on your application.
-    if (typeof obj !== "object") return true;
-
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
-
-    return true;
-}
+*/
